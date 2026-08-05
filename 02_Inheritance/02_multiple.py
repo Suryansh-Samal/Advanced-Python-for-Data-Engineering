d000ce9b41487b6ab4e9505f21d925ee24365e63
Suryansh Samal <suryanshsamal4@gmail.com>
2026-08-05 19:05:06 +0530

    Added Multiple inheritance

---
+++ 02_Inheritance/02_multiple.py
@@ -0,0 +1,30 @@
+class Company:
+
+    def __init__(self, company_name):
+        self.company_name = company_name
+
+    def company_info(self):
+        return f'{self.company_name}'
+
+class Department:
+
+    def __init__(self, department_name):
+        self.department_name = department_name
+
+    def department_info(self):
+        return f'{self.department_name}'
+
+class Employee(Company, Department):
+
+    def __init__(self,employee_name, department_name, company_name):
+       self.employee_name = employee_name
+       Department.__init__(self, department_name)
+       Company.__init__(self, company_name)
+
+    def employee_info(self):
+        company = Company.company_info(self)
+        department = Department.department_info(self)
+        print(f'Employee: {self.employee_name}\nDepartment: {department}\nCompany: {company}')
+
+emp1 = Employee('Suryansh', 'Data', 'Apple')
+emp1.employee_info()
