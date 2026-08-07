34f7bcbe6c1cb144c21f43fd5a563e20884d7dcc
Suryansh Samal <suryanshsamal4@gmail.com>
2026-08-07 17:42:36 +0530

    Added multiple tasks example

---
+++ 06_Async/04_multiple_tasks.py
@@ -0,0 +1,33 @@
+import asyncio
+import time
+
+#first task
+async def api_call(url:str, delay:int):
+    print(f"fetching data from {url}...")
+    await asyncio.sleep(delay)  # Simulate an API call that takes the specified delay
+    print(f"data fetched from {url}")
+    return f"{url} data"
+
+#second task
+async def execution():
+    time.sleep(5)  # Simulate a blocking operation that takes 5 seconds
+    print("Task execution completed")
+
+#third task
+async def transformation():
+    asyncio.sleep(2)  # Simulate a non-blocking operation that takes 2 seconds
+    print("Data transformation completed")
+
+
+async def main():
+    await asyncio.gather(
+    api_call("https://api.example.com/data1", 2),
+    execution(),
+    transformation()
+)
+
+    print("All the tasks are completed")
+ 
+
+# Run the async function
+asyncio.run(main())
